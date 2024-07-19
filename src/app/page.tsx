9223e149ec095f778fa005a7f4cfadf10eb019ea
Eduardo V. Micami <eduvns.dev@gmail.com>
2024-07-18 23:07:22 -0300

    Adicionado o campo de entrada de valores e o botão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-import Image from "next/image";
+"use client"
+
+import { CurrencyInput } from 'react-currency-mask';
+import { Button, TextField } from '@mui/material';
 
 export default function Home() {
   return (
-    <h1>APP DO PÃO DURO</h1>
+    <main className="">
+      <h1 className="flex justify-center text-3xl py-3 font-bold">App do Pão Duro</h1>
+      <div className="flex justify-center py-3">
+        <CurrencyInput
+          onChangeValue={(event, originalValue, maskedValue) => {
+            console.log(event, originalValue, maskedValue);
+          }}
+          InputElement={<TextField label="Custom Input" size="small" />}
+        />
+        <Button className="ml-3" variant="contained">Gastei</Button>
+      </div>
+    </main>
   );
 }
